07fe04d347b7d9bd5d472e411d4ede7d1aa4be36
Denis Trenin <koteika360@yandex.ru>
2023-07-22 00:02:17 +0300

    it api now, BEACH

--- first_file.py
+++ first_file.py
@@ -1,5 +1,10 @@
-print('Hello GitHub')
+import requests
 
-print('Assalyamu Aleykum')
+api_url = 'http://api.open-notify.org/iss-now.json'
 
-print('jopa')
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)    # При другом коде ответа выводим этот код
